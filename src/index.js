0bc3469f3fc3c98b46b44891f5ad44371b43dda4
Alisson Chiquitto <chiquitto@gmail.com>
2021-01-22 15:51:53 -0300

    Projeto pronto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </React.Fragment>,
   document.getElementById('root')
 );
 
